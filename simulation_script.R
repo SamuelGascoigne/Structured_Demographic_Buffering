d1966c66458775da747ba20b21ab2e21b54f7552
Samuel Gascoigne <pemb6235@OX.AC.UK>
2023-03-01 12:15:22 +0000

    new commit

--- simulation_script.R
+++ simulation_script.R
@@ -7,30 +7,23 @@ source("omnibus_simulation_function.R")
 
 
 Genus_species <- c("Berberis_thunbergii", 
-                   #"Heliconia_tortuosa", 
-                   #"Calathea_crotalifera"
-                   ) 
+                   "Heliconia_tortuosa", 
+                   "Calathea_crotalifera") 
+
  
+variance_values <- seq(from = 0.9, to = 1.1, length.out = 15)
  
-# variance_values <- seq(from = 0.9, to = 1.1, length.out = 15)
-#  
-# autocorrelation_values <- seq(from = -0.95, to = 0.95, length.out = 15)
+autocorrelation_values <- seq(from = -0.8, to = 0.8, length.out = 15)
+
+beta_simulation <- omnibus_simulation(Genus_species, variance_values, autocorrelation_values)
+
+beta_simulation$df
+
+
+# variance_values <- seq(from = 0.9, to = 1.1, length.out = 1)
+# 
+# autocorrelation_values <- seq(from = -0.95, to = 0.95, length.out = 1)
 # 
 # beta_simulation <- omnibus_simulation(Genus_species, variance_values, autocorrelation_values)
 # 
 # beta_simulation$df
-
-Genus_species <- c("Berberis_thunbergii") 
-
-variance_values <- seq(from = 0.9, to = 1.1, length.out = 35)
-
-autocorrelation_values <- seq(from = -0.95, to = 0.95, length.out = 35)
-
-seed_vec <- c(1, 2, 3, 4, 5)
-
-beta_simulation <- omnibus_simulation(Genus_species, 
-                                      variance_values, 
-                                      autocorrelation_values,
-                                      seed_vector = seed_vec)
-
-beta_simulation$df
