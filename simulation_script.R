9c5f2adf341a78d7115c96865f3b5ad0802946b6
SamuelGascoigne <samueljlgascoigne@gmail.com>
2024-02-12 14:30:21 -0600

    Update to simulation script

--- simulation_script.R
+++ simulation_script.R
@@ -1,5 +1,3 @@
-# Testing
-
 source("packages_and_setup.R")
 source("analysis_functions.R")
 source("individual_population_simulation_functions.R")
@@ -20,10 +18,3 @@ beta_simulation <- omnibus_simulation(Genus_species, variance_values, autocorrel
 beta_simulation$df
 
 
-# variance_values <- seq(from = 0.9, to = 1.1, length.out = 1)
-# 
-# autocorrelation_values <- seq(from = -0.95, to = 0.95, length.out = 1)
-# 
-# beta_simulation <- omnibus_simulation(Genus_species, variance_values, autocorrelation_values)
-# 
-# beta_simulation$df
